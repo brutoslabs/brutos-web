46018257a79ccb4c610b4aef0ca7bff724f4efea
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2010-11-30 02:23:26 +0000

    .

--- brutos-core/src/main/java/org/brandao/brutos/http/DataInput.java
+++ brutos-core/src/main/java/org/brandao/brutos/http/DataInput.java
@@ -99,5 +99,6 @@ public class DataInput {
         }
         //request.setAttribute( BrutosConstants.WEBFRAME, wf );
         scope.put( BrutosConstants.WEBFRAME, wf );
+        scope.put( BrutosConstants.CONTROLLER, wf );
     }
 }
