95511a9face102779f97b2039cbbda792cd696f5
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2011-01-16 01:18:58 +0000

    Atualização da documentação

--- trunk/brutos-core/src/main/java/org/brandao/brutos/type/Type.java
+++ trunk/brutos-core/src/main/java/org/brandao/brutos/type/Type.java
@@ -21,78 +21,21 @@ import java.io.IOException;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.brandao.brutos.scope.Scope;
 
 /**
- * Allows the creation types.
  *
  * @author Afonso Brandao
  */
 public interface Type {
 
-    /**
-     * Converts a value to a defined type.
-     * 
-     * @param request Request object.
-     * @param context Context object.
-     * @param value Value to be converted. If the
-     * value comes from the request is a string
-     * otherwise you do not need the conversion.
-     * @return Converted value.
-     */
     public Object getValue( HttpServletRequest request, ServletContext context, Object value );
 
-    /**
-     * Converts a value to a defined type.
-     *
-     * @param value Value to be converted. If the
-     * value comes from the request is a string
-     * otherwise you do not need the conversion.
-     * @return Converted value.
-     */
     public Object getValue( Object value );
 
-    /**
-     * Converts the value to be sent by the response object.
-     * @param response Response object.
-     * @param context Context object.
-     * @param value Value to be converted.
-     * @throws IOException Thrown if there is a problem to send.
-     */
     public void setValue( HttpServletResponse response, ServletContext context, Object value ) throws IOException;
 
-    /**
-     * Converts the value to be sent by the response object.
-     * @param value Value to be converted.
-     * @throws IOException Thrown if there is a problem to send.
-     */
     public void setValue( Object value ) throws IOException;
 
-    /**
-     * Get the type of class
-     * @return Class type.
-     */
     public Class getClassType();
-
-    /*
-     * Converts a value to a defined type.
-     * @since 1.4
-     * @param scope Scope.
-     * @param name Value name.
-     * @param souce Source.
-     * @return Converted value.
-     */
-    //public Object getValue( Scope scope, String name, Object source );
-
-    /*
-     * Converts the value to be sent by the response.
-     * @since 1.4
-     * @param scope Scope.
-     * @param name Value name.
-     * @param souce Source.
-     * @param value Value.
-     * @throws IOException Thrown if there is a problem to send.
-     */
-    //public void setValue( Scope scope, String name, Object source, Object value ) throws IOException;
 
 }
