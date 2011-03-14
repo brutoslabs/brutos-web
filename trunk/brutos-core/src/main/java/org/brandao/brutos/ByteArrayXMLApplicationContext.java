28c5381abde342e4d7b129dcbd617cd6d04cc1b9
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2011-03-14 01:31:20 +0000

    atualização da interface xml

---
+++ trunk/brutos-core/src/main/java/org/brandao/brutos/ByteArrayXMLApplicationContext.java
@@ -0,0 +1,54 @@
+/*
+ * Brutos Web MVC http://brutos.sourceforge.net/
+ * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * This library is free software. You can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (GPL) version 3.0 or (at your option) any later
+ * version.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ */
+
+package org.brandao.brutos;
+
+import org.brandao.brutos.io.ByteArrayResource;
+import org.brandao.brutos.io.Resource;
+import org.brandao.brutos.xml.AbstractXMLApplicationContext;
+
+/**
+ *
+ * @author Brandao
+ */
+public class ByteArrayXMLApplicationContext
+        extends AbstractXMLApplicationContext{
+
+    private Resource[] resources;
+
+    public ByteArrayXMLApplicationContext( byte[][] arrays,
+            ApplicationContext parent ){
+        super( parent );
+
+        resources = new Resource[arrays.length];
+        for( int i=0;i<arrays.length;i++ )
+            resources[i] = new ByteArrayResource( arrays[i] );
+    }
+
+    public ByteArrayXMLApplicationContext( byte[][] array ){
+        this(array,null);
+    }
+
+    public ByteArrayXMLApplicationContext( byte[] array ){
+        this(new byte[][]{array}, null);
+    }
+
+    protected Resource[] getContextResources() {
+        return resources;
+    }
+
+}
