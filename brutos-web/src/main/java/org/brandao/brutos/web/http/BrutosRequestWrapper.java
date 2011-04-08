1edad1984a68e8e205f90a2fb7bed15ac9aca318
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2011-04-08 04:41:24 +0000

    #

---
+++ brutos-web/src/main/java/org/brandao/brutos/web/http/BrutosRequestWrapper.java
@@ -0,0 +1,74 @@
+/*
+ * Brutos Web MVC http://brutos.sourceforge.net/
+ * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * This library is free software. You can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (GPL) version 3.0 or (at your option) any later
+ * version.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ */
+
+package org.brandao.brutos.web.http;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestWrapper;
+
+/**
+ *
+ * @author Brandao
+ */
+public class BrutosRequestWrapper extends ServletRequestWrapper implements BrutosRequest{
+
+    private BrutosRequest request;
+
+    public BrutosRequestWrapper( BrutosRequest request ){
+        super( (ServletRequest)request );
+        this.request = request;
+    }
+
+    public Object getObject(String name) {
+        return request.getObject(name);
+    }
+
+    public List<Object> getObjects(String name) {
+        return request.getObjects(name);
+    }
+
+    public UploadListener getUploadListener() {
+        return request.getUploadListener();
+    }
+
+    public void parseRequest() throws IOException {
+        request.parseRequest();
+    }
+
+    public ServletRequest getServletRequest() {
+        return request.getServletRequest();
+    }
+
+    public void setParameter(String name, String value) {
+        request.setParameter(name, value);
+    }
+
+    public void setParameters(String name, String[] values) {
+        request.setParameters(name, values);
+    }
+
+    public void setObject(String name, Object value) {
+        request.setObject(name, value);
+    }
+
+    public void setObjects(String name, Object[] value) {
+        request.setObjects(name, value);
+    }
+
+}
