eb09873e26247c8909c3f9654e7101a8d33a802f
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2011-05-21 20:12:01 +0000

    #

--- trunk/brutos-web/src/test/java/org/brandao/brutos/helper/controller/FactoryBean.java
+++ trunk/brutos-web/src/test/java/org/brandao/brutos/helper/controller/FactoryBean.java
@@ -27,6 +27,13 @@ public class FactoryBean {
         return new SimpleBean();
     }
 
+    public SimpleBean getInstance( String arg ){
+        if( arg != null )
+            return new SimpleBean(arg);
+        else
+            return null;
+    }
+
     public static SimpleBean getInstanceByStaticMethod(){
         return new SimpleBean();
     }
