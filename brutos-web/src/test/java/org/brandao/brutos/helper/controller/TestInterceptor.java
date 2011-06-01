973aed009e1bd094257a369992235b57f7328444
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2011-06-01 03:35:17 +0000

    #

---
+++ brutos-web/src/test/java/org/brandao/brutos/helper/controller/TestInterceptor.java
@@ -0,0 +1,48 @@
+/*
+ * Brutos Web MVC http://brutos.sourceforge.net/
+ * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * This library is free software. You can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (GPL) version 3.0 or (at your option) any later
+ * version.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ */
+
+
+package org.brandao.brutos.helper.controller;
+
+import org.brandao.brutos.ScopeType;
+import org.brandao.brutos.Scopes;
+import org.brandao.brutos.interceptor.AbstractInterceptor;
+import org.brandao.brutos.interceptor.InterceptedException;
+import org.brandao.brutos.interceptor.InterceptorHandler;
+import org.brandao.brutos.interceptor.InterceptorStack;
+import org.brandao.brutos.scope.Scope;
+import org.brandao.brutos.web.ContextLoader;
+
+/**
+ *
+ * @author Brandao
+ */
+public class TestInterceptor extends AbstractInterceptor{
+
+    public void intercepted(InterceptorStack stack, InterceptorHandler handler)
+            throws InterceptedException {
+
+        Scopes scopes = ContextLoader.getCurrentWebApplicationContext().getScopes();
+        Scope scope = scopes.get(ScopeType.REQUEST);
+        for(String key: this.props.keySet()){
+            scope.put(key, this.props.get(key));
+        }
+
+        stack.next(handler);
+    }
+
+}
