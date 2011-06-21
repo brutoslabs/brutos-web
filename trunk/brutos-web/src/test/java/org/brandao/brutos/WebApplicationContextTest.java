865ca2b7e3590daaa9fa6523fb5ba3531fd04995
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2011-06-21 03:32:16 +0000

    #

---
+++ trunk/brutos-web/src/test/java/org/brandao/brutos/WebApplicationContextTest.java
@@ -0,0 +1,125 @@
+/*
+ * Brutos Web MVC http://brutos.sourceforge.net/
+ * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * This library is free software. You can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (GPL) version 3.0 or (at your option) any later
+ * version.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ */
+
+package org.brandao.brutos;
+
+import java.util.Properties;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import org.brandao.brutos.helper.controller.AbstractTester;
+import org.brandao.brutos.helper.controller.SimpleBean;
+import org.brandao.brutos.helper.controller.TypeTest;
+import org.brandao.brutos.io.ClassPathResource;
+import org.brandao.brutos.io.Resource;
+import org.brandao.brutos.scope.Scope;
+import org.brandao.brutos.type.Type;
+import org.brandao.brutos.type.Types;
+import org.brandao.brutos.web.ConfigurableWebApplicationContext;
+import org.brandao.brutos.web.GenericXMLWebApplicationContext;
+
+/**
+ *
+ * @author Brandao
+ */
+public class WebApplicationContextTest extends AbstractTester implements Test{
+
+    public ConfigurableWebApplicationContext getApplicationContext(String resourceName) {
+        return new GenericXMLWebApplicationContext(
+                new Resource[]{
+                    new ClassPathResource( 
+                            getClass(),
+                            resourceName )});
+    }
+
+    public void testContext1(){
+        super.execTest(
+            new HandlerTest(){
+
+                public String getResourceName() {
+                    return
+                        "org/brandao/brutos/xml/helper/context/context-test1.xml";
+                }
+
+                public void run(ConfigurableApplicationContext app,
+                        HttpServletRequest request, HttpServletResponse response) {
+
+                    Properties prop = app.getConfiguration();
+                    TestCase.assertEquals("value", prop.getProperty("teste"));
+                    TestCase.assertEquals("value2", prop.getProperty("teste2"));
+                }
+            
+        });
+    }
+
+    public void testContext2(){
+        super.execTest(
+            new HandlerTest(){
+
+                public String getResourceName() {
+                    return
+                        "org/brandao/brutos/xml/helper/context/context-test2.xml";
+                }
+
+                public void run(ConfigurableApplicationContext app,
+                        HttpServletRequest request, HttpServletResponse response) {
+
+                    Type type = Types.getType(SimpleBean.class);
+                    TestCase.assertTrue(type instanceof TypeTest);
+                }
+
+        });
+    }
+
+    public void testContext3(){
+        super.execTest(
+            new HandlerTest(){
+
+                public String getResourceName() {
+                    return
+                        "org/brandao/brutos/xml/helper/context/context-test3.xml";
+                }
+
+                public void run(ConfigurableApplicationContext app,
+                        HttpServletRequest request, HttpServletResponse response) {
+
+                    Type type = Types.getType(SimpleBean.class);
+                    TestCase.assertTrue(type instanceof TypeTest);
+                }
+
+        });
+    }
+
+    public void testContext4(){
+        super.execTest(
+            new HandlerTest(){
+
+                public String getResourceName() {
+                    return
+                        "org/brandao/brutos/xml/helper/context/context-test4.xml";
+                }
+
+                public void run(ConfigurableApplicationContext app,
+                        HttpServletRequest request, HttpServletResponse response) {
+
+                }
+
+        });
+    }
+
+}
