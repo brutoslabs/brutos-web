65fa3cdf44ea15831e751bc9b45a1ed7af09ac67
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2011-07-10 01:31:58 +0000

    #

---
+++ trunk/brutos-core/src/main/java/org/brandao/brutos/validator/MaxlengthValidationRule.java
@@ -0,0 +1,45 @@
+/*
+ * Brutos Web MVC http://brutos.sourceforge.net/
+ * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * This library is free software. You can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (GPL) version 3.0 or (at your option) any later
+ * version.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ */
+
+package org.brandao.brutos.validator;
+
+import java.util.Properties;
+import org.brandao.brutos.type.Type;
+import org.brandao.brutos.type.Types;
+
+/**
+ *
+ * @author Brandao
+ */
+public class MaxlengthValidationRule implements ValidationRule{
+
+    private Type integerType = Types.getType(Integer.class);
+
+    public void validate(Properties config, Object source, Object value) {
+        if( value instanceof String ){
+            if( config.containsKey( RestrictionRules.MAXLENGTH.toString() ) ){
+                Number tmp = (Number) integerType
+                                .getValue(
+                                config.get(RestrictionRules.MAXLENGTH.toString()));
+
+                if( ((String)value).length() > tmp.intValue() )
+                    throw new ValidatorException();
+            }
+        }
+    }
+
+}
