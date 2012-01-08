5384b86a038dd3006f9b5f7129b91949bd3dc24f
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2012-01-08 13:49:39 +0000

    #

--- brutos-core/src/main/java/org/brandao/brutos/DefaultMvcRequest.java
+++ brutos-core/src/main/java/org/brandao/brutos/DefaultMvcRequest.java
@@ -1,8 +1,19 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Brutos Web MVC http://brutos.sourceforge.net/
+ * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * This library is free software. You can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (GPL) version 3.0 or (at your option) any later
+ * version.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
  */
-
 package org.brandao.brutos;
 
 import java.io.IOException;
