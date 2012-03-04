8501bfd46631d273e9f42ac7eb4f1cea43cf3b87
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2012-03-04 20:14:42 +0000

    #

--- brutos-web/src/test/java/org/brandao/brutos/WebApplicationMappingBeanTest.java
+++ brutos-web/src/test/java/org/brandao/brutos/WebApplicationMappingBeanTest.java
@@ -23,8 +23,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import junit.framework.Test;
