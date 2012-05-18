6c8e7a0b4e363f1ee83be09c826e3eeff158eaf4
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2012-05-18 03:43:25 +0000

    #

--- trunk/brutos-core/src/main/java/org/brandao/brutos/xml/XMLBrutosConstants.java
+++ trunk/brutos-core/src/main/java/org/brandao/brutos/xml/XMLBrutosConstants.java
@@ -25,8 +25,8 @@ public interface XMLBrutosConstants {
     
     public static final String JAXP_SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
     public static final String XML_BRUTOS_SCHEMA = "brutos_1_1.xsd";
-    public static final String XML_BRUTOS_CONTEXT_SCHEMA = "brutos-context-1.0.xsd";
-    public static final String XML_BRUTOS_CONTROLLER_SCHEMA = "brutos-controllers-1.0.xsd";
+    public static final String XML_BRUTOS_CONTEXT_SCHEMA = "brutos-context-1.1.xsd";
+    public static final String XML_BRUTOS_CONTROLLER_SCHEMA = "brutos-controllers-1.1.xsd";
     public static final String JAXP_SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
     public static final String W3C_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
     public static final String XML_BRUTOS = "brutos-configuration";
