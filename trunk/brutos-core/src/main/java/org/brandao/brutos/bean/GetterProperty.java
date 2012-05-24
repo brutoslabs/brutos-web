144564f45efd8393c008e5d507ef324cedd8faaa
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2012-05-24 03:48:00 +0000

    #9

--- trunk/brutos-core/src/main/java/org/brandao/brutos/bean/GetterProperty.java
+++ trunk/brutos-core/src/main/java/org/brandao/brutos/bean/GetterProperty.java
@@ -1,18 +1,18 @@
 /*
- * Brutos Web MVC http://brutos.sourceforge.net/
+ * Brutos Web MVC http://www.brutosframework.com.br/
  * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
  *
- * This library is free software. You can redistribute it
- * and/or modify it under the terms of the GNU General Public
- * License (GPL) version 3.0 or (at your option) any later
- * version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.gnu.org/licenses/gpl.html
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * Distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.brandao.brutos.bean;
@@ -30,11 +30,10 @@ public class GetterProperty {
     private Object object;
     private Method method;
 
-    /**
-     * @deprecated 
-     * @param field
-     * @param object
-     */
+    public GetterProperty( Field field, Method method, Object object ){
+        
+    }
+    
     public GetterProperty( Field field, Object object ){
         this.field = field;
         this.object = object;
@@ -45,20 +44,27 @@ public class GetterProperty {
         this.object = object;
     }
 
+    public Object get(Object o) throws Exception{
+        return field != null? field.get(o) : method.invoke( o, new Object[]{} );
+    }
+    
     public Object get() throws Exception{
-        /*
-        String fieldName = field.getName();
-        String pre = field.getType() == Boolean.TYPE? "is" : "get";
-        String methodName = pre +
-                            String.valueOf( fieldName.charAt( 0 ) ).toUpperCase() +
-                            fieldName.substring( 1, fieldName.length() );
-
-        Method getter = field.getDeclaringClass().getMethod( methodName );
-        */
-        return method.invoke( object, new Object[]{} );
+        return get(object);
     }
 
     public Method getMethod(){
         return method;
+    }
+    
+    public Field getField(){
+        return this.field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
     }
 }
