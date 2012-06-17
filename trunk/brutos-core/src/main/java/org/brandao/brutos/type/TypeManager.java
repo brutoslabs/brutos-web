6630b2607013dd8b5aa4d3d0ae9a964c4a065dec
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2012-06-17 07:18:27 +0000

    #9 #32

--- trunk/brutos-core/src/main/java/org/brandao/brutos/type/TypeManager.java
+++ trunk/brutos-core/src/main/java/org/brandao/brutos/type/TypeManager.java
@@ -77,7 +77,7 @@ public class TypeManager {
     
    public static boolean isStandardType(Class clazz){
        Type type = getType(clazz);
-       return type == null || type instanceof ObjectType;
+       return !(type == null || type instanceof ObjectType);
    }
    
     public static Type getType( Object classType ){
