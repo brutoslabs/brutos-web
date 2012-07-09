6f9d9bf7f6d05a484e5d763764cff1c32d1fef89
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2012-07-09 12:53:18 +0000

    #9

---
+++ trunk/brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/ControllerTest19Controller.java
@@ -0,0 +1,110 @@
+/*
+ * Brutos Web MVC http://www.brutosframework.com.br/
+ * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.brandao.brutos.annotation.helper;
+
+import java.util.Date;
+import org.brandao.brutos.annotation.Enumerated;
+import org.brandao.brutos.annotation.EnumerationType;
+import org.brandao.brutos.annotation.Identify;
+import org.brandao.brutos.annotation.Temporal;
+
+/**
+ *
+ * @author Brandao
+ */
+public class ControllerTest19Controller {
+    
+    private int propertyA;
+
+    private String propertyB;
+    
+    private Date propertyC;
+    
+    private Date propertyD;
+    
+    private EnumTest propertyE;
+
+    private EnumTest propertyF;
+    
+    private EnumTest propertyG;
+
+    public int getPropertyA() {
+        return propertyA;
+    }
+
+    public void setPropertyA(int propertyA) {
+        this.propertyA = propertyA;
+    }
+
+    @Identify(bean="prop")
+    public String getPropertyB() {
+        return propertyB;
+    }
+
+    public void setPropertyB(String propertyB) {
+        this.propertyB = propertyB;
+    }
+
+    public Date getPropertyC() {
+        return propertyC;
+    }
+
+    public void setPropertyC(Date propertyC) {
+        this.propertyC = propertyC;
+    }
+
+    @Temporal("yyyy-MM-dd")
+    public Date getPropertyD() {
+        return propertyD;
+    }
+
+    public void setPropertyD(Date propertyD) {
+        this.propertyD = propertyD;
+    }
+
+    public EnumTest getPropertyE() {
+        return propertyE;
+    }
+
+    public void setPropertyE(EnumTest propertyE) {
+        this.propertyE = propertyE;
+    }
+
+    @Enumerated(value=EnumerationType.ORDINAL)
+    public EnumTest getPropertyF() {
+        return propertyF;
+    }
+
+    public void setPropertyF(EnumTest propertyF) {
+        this.propertyF = propertyF;
+    }
+
+    @Enumerated(value=EnumerationType.STRING)
+    public EnumTest getPropertyG() {
+        return propertyG;
+    }
+
+    public void setPropertyG(EnumTest propertyG) {
+        this.propertyG = propertyG;
+    }
+    
+    public Object myFirstAction(){
+        return null;
+    }
+
+}
