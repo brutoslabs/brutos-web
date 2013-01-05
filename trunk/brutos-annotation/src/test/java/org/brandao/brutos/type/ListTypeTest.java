7332c4f440b49833e50b819d0313e8d284676b6c
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2013-01-05 15:46:43 +0000

    #9

---
+++ trunk/brutos-annotation/src/test/java/org/brandao/brutos/type/ListTypeTest.java
@@ -0,0 +1,51 @@
+/*
+ * Brutos Web MVC http://www.brutosframework.com.br/
+ * Copyright (C) 2009 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.brandao.brutos.type;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Brandao
+ */
+public class ListTypeTest extends TestCase{
+
+    public void test1(){
+        ParameterizedType classType = new ParameterizedType(){
+
+            public Type[] getActualTypeArguments() {
+                return new Type[]{Integer.class};
+            }
+
+            public Type getRawType() {
+                return List.class;
+            }
+
+            public Type getOwnerType() {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+            
+        };
+        
+        CollectionType type = (CollectionType) TypeManager.getType(classType,null,null);
+        type.setGenericType(classType);
+    }
+}
