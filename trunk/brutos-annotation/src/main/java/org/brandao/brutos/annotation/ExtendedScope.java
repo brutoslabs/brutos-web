0bec7561b42a8a22e94229e6ae3f449ef3dc8da5
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2014-02-04 00:00:57 +0000

    #

---
+++ trunk/brutos-annotation/src/main/java/org/brandao/brutos/annotation/ExtendedScope.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.brandao.brutos.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Indica um no escopo.
+ * 
+ * @author Cliente
+ */
+@java.lang.annotation.Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExtendedScope {
+    
+    /**
+     * Identificação do escopo.
+     */
+    String value();
+    
+}
