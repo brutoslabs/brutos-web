f063b10cb59dc16661133d9e35cac757d845851e
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2014-03-29 20:29:30 +0000

    #

--- brutos-core/src/test/java/org/brandao/brutos/validator/ValidatorProviderTest.java
+++ brutos-core/src/test/java/org/brandao/brutos/validator/ValidatorProviderTest.java
@@ -349,23 +349,30 @@ public class ValidatorProviderTest extends TestCase{
 
     public static class CustomValidationRule implements ValidationRule{
 
-        public void validate(Properties config, Object source, Object value)
+        private Integer start;
+        
+        private Integer end;
+        
+        public void validate(Object source, Object value)
                 throws ValidatorException {
 
             if( !(value instanceof Number) )
                 throw new ValidatorException("invalid type: " +  value.getClass());
 
+            Integer val = new Integer(((Number)value).intValue());
+            if( val.intValue() < start.intValue() || val.intValue() > end.intValue() )
+                throw new ValidatorException();
+        }
+
+        public void setConfiguration(Properties config) {
             String range = config.getProperty("range");
+            
             if( !range.matches("\\d+-\\d+") )
                 throw new ValidatorException("invalid range syntax: " +  range );
 
             String[] vals = range.split("-");
-            Integer start = Integer.valueOf(vals[0]);
-            Integer end = Integer.valueOf(vals[1]);
-
-            Integer val = new Integer(((Number)value).intValue());
-            if( val.intValue()<start.intValue() || val.intValue()>end.intValue() )
-                throw new ValidatorException();
+            this.start = Integer.valueOf(vals[0]);
+            this.end = Integer.valueOf(vals[1]);
         }
 
     }
