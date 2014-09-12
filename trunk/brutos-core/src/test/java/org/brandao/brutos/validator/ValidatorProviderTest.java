80ce91e5d0060e426cc08d1de0015cbc022bd812
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2014-09-12 00:39:12 +0000

    #

--- trunk/brutos-core/src/test/java/org/brandao/brutos/validator/ValidatorProviderTest.java
+++ trunk/brutos-core/src/test/java/org/brandao/brutos/validator/ValidatorProviderTest.java
@@ -331,33 +331,4 @@ public class ValidatorProviderTest extends TestCase{
 
     }
 
-    public static class CustomValidationRule implements ValidationRule{
-
-        private Integer start;
-        
-        private Integer end;
-        
-        public void validate(Object source, Object value)
-                throws ValidatorException {
-
-            if( !(value instanceof Number) )
-                throw new ValidatorException("invalid type: " +  value.getClass());
-
-            Integer val = new Integer(((Number)value).intValue());
-            if( val.intValue() < start.intValue() || val.intValue() > end.intValue() )
-                throw new ValidatorException();
-        }
-
-        public void setConfiguration(Properties config) {
-            String range = config.getProperty("range");
-            
-            if( !range.matches("\\d+-\\d+") )
-                throw new ValidatorException("invalid range syntax: " +  range );
-
-            String[] vals = range.split("-");
-            this.start = Integer.valueOf(vals[0]);
-            this.end = Integer.valueOf(vals[1]);
-        }
-
-    }
 }
