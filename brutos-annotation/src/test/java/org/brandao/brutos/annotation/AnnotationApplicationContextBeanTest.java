bafd06ec5e1dafbb6c857556c699160e843fb3dd
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2014-12-13 12:12:20 +0000

    #

--- brutos-annotation/src/test/java/org/brandao/brutos/annotation/AnnotationApplicationContextBeanTest.java
+++ brutos-annotation/src/test/java/org/brandao/brutos/annotation/AnnotationApplicationContextBeanTest.java
@@ -42,7 +42,6 @@ public class AnnotationApplicationContextBeanTest
     extends AbstractWebAnnotationApplicationContextTest{
 
     public AnnotationApplicationContextBeanTest(){
-        //TypeManager.remove(List.class);
     }
     
     @Override
