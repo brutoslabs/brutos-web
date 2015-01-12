afdcc398e3b9a05fd3ecb891b4cad47df29283a5
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2015-01-12 00:05:23 +0000

    #

--- trunk/brutos-annotation/src/main/java/org/brandao/brutos/annotation/web/test/MockAnnotationWebApplicationContext.java
+++ trunk/brutos-annotation/src/main/java/org/brandao/brutos/annotation/web/test/MockAnnotationWebApplicationContext.java
@@ -21,11 +21,11 @@ package org.brandao.brutos.annotation.web.test;
 import java.util.Properties;
 import org.brandao.brutos.BrutosConstants;
 import org.brandao.brutos.annotation.web.AnnotationWebApplicationContext;
+import org.brandao.brutos.io.ByteArrayResource;
+import org.brandao.brutos.io.Resource;
 import org.brandao.brutos.test.MockObjectFactory;
 import org.brandao.brutos.test.MockRenderView;
-import org.brandao.brutos.test.MockScope;
 import org.brandao.brutos.validator.JSR303ValidatorFactory;
-import org.brandao.brutos.web.WebScopeType;
 import org.brandao.brutos.web.test.MockWebInvoker;
 
 /**
@@ -35,22 +35,14 @@ import org.brandao.brutos.web.test.MockWebInvoker;
 public class MockAnnotationWebApplicationContext 
     extends AnnotationWebApplicationContext{
 
-    @Override
-    protected void initScopes(){
-        getScopes().register( WebScopeType.APPLICATION.toString(),
-                new MockScope() );
-        getScopes().register( WebScopeType.FLASH.toString(),
-                new MockScope() );
-        getScopes().register( WebScopeType.IOC.toString(),
-                new MockScope() );
-        getScopes().register( WebScopeType.REQUEST.toString(),
-                new MockScope() );
-        getScopes().register( WebScopeType.SESSION.toString(),
-                new MockScope() );
-        getScopes().register( WebScopeType.PARAM.toString(),
-                new MockScope() );
-    }
+    public static final String IGNORE_RESOURCES = "org.brandao.brutos.mock.ignore_resources";
 
+    public static final String XML_CONTENT = "org.brandao.brutos.mock.xml_content";
+    
+    private boolean ignoreResources;
+    
+    private String resource;
+    
     @Override
     protected void overrideConfig(){
         Properties config = this.getConfiguration();
@@ -59,6 +51,16 @@ public class MockAnnotationWebApplicationContext
         config.put(BrutosConstants.OBJECT_FACTORY_CLASS, MockObjectFactory.class.getName());
         config.put(BrutosConstants.VALIDATOR_FACTORY_CLASS, JSR303ValidatorFactory.class.getName());
         super.overrideConfig();
+        
+        this.ignoreResources = "true".equals(config.getProperty(IGNORE_RESOURCES, "false"));
+        this.resource = config.getProperty(XML_CONTENT);
+        
+        if(this.resource != null)
+            super.setResources(new Resource[]{new ByteArrayResource(this.resource.getBytes())});
+        
+        if(this.ignoreResources)
+            super.setLocations(new String[]{});
+        
     }
     
 }
