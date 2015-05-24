db5c73d8798c92a103cc22fc4afb5d676440ecb9
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2015-05-24 19:27:05 +0000

    #

--- trunk/brutos-core/src/main/java/org/brandao/brutos/scanner/vfs/JbossVFSURLType.java
+++ trunk/brutos-core/src/main/java/org/brandao/brutos/scanner/vfs/JbossVFSURLType.java
@@ -18,6 +18,8 @@
 package org.brandao.brutos.scanner.vfs;
 
 import java.net.URL;
+import java.util.List;
+
 import org.jboss.vfs.VirtualFile;
 
 /**
@@ -29,7 +31,12 @@ public class JbossVFSURLType implements URLType{
     public Dir toDir(URL url) throws Exception {
         VirtualFile vf = null;
         try{ 
-            vf = (VirtualFile) url.openConnection().getContent(); 
+            vf = (VirtualFile) url.openConnection().getContent();
+            List<VirtualFile> lvf = vf.getChildrenRecursively();
+            
+            for(VirtualFile v: lvf)
+            	v.getPhysicalFile();
+            
             java.io.File file = 
                 new java.io.File(
                 vf.getPhysicalFile().getParentFile(), 
