0db3cfe8d31e7bf2ced3ab956cf4e59f5969b7c2
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2015-06-04 17:05:40 +0000

    #

---
+++ trunk/brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/elementcollection/fail/ControllerElementCollectionPropertyFailTest.java
@@ -0,0 +1,11 @@
+package org.brandao.brutos.annotation.helper.elementcollection.fail;
+
+import java.util.List;
+
+import org.brandao.brutos.annotation.ImportBeans;
+
+public class ControllerElementCollectionPropertyFailTest {
+
+	private List property;
+	
+}
