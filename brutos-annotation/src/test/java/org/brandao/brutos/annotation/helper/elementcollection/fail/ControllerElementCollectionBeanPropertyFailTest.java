78a04e657526223c1610fdedde6ba97d0f39fed4
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2015-06-04 17:18:56 +0000

    #

---
+++ brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/elementcollection/fail/ControllerElementCollectionBeanPropertyFailTest.java
@@ -0,0 +1,7 @@
+package org.brandao.brutos.annotation.helper.elementcollection.fail;
+
+public class ControllerElementCollectionBeanPropertyFailTest {
+
+	private ElementCollectionPropertyFailTest property;
+	
+}
