401d0fe5c678c3ffd4584d9211fd86ef2e94dc2c
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2015-06-10 15:57:06 +0000

    #

--- brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/elementcollection/fail/ControllerElementCollectionBeanPropertyFailTest.java
+++ brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/elementcollection/fail/ControllerElementCollectionBeanPropertyFailTest.java
@@ -1,5 +1,8 @@
 package org.brandao.brutos.annotation.helper.elementcollection.fail;
 
+import org.brandao.brutos.annotation.Controller;
+
+@Controller
 public class ControllerElementCollectionBeanPropertyFailTest {
 
 	private ElementCollectionPropertyFailTest property;
--- brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/elementcollection/fail/ControllerElementCollectionPropertyFailTest.java
+++ brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/elementcollection/fail/ControllerElementCollectionPropertyFailTest.java
@@ -2,6 +2,9 @@ package org.brandao.brutos.annotation.helper.elementcollection.fail;
 
 import java.util.List;
 
+import org.brandao.brutos.annotation.Controller;
+
+@Controller
 public class ControllerElementCollectionPropertyFailTest {
 
 	public List getProperty(){
