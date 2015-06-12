ed5d799f52375b635d4c4248911295c6a7be42c3
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2015-06-12 12:32:29 +0000

    #

---
+++ brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/keycollection/fail/ControllerKeyCollectionFieldFailTest.java
@@ -0,0 +1,12 @@
+package org.brandao.brutos.annotation.helper.keycollection.fail;
+
+import java.util.Map;
+
+import org.brandao.brutos.annotation.Controller;
+
+@Controller
+public class ControllerKeyCollectionFieldFailTest {
+
+	private Map property;
+	
+}
---
+++ brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/keycollection/fail/KeyCollectionConstructorFailTest.java
@@ -0,0 +1,10 @@
+package org.brandao.brutos.annotation.helper.keycollection.fail;
+
+import java.util.Map;
+
+public class KeyCollectionConstructorFailTest {
+
+	public KeyCollectionConstructorFailTest(Map list){
+	}
+	
+}
