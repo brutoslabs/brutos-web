ca23d63d20641a423335bf0df8beaa725fa755f3
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2015-06-13 23:31:34 +0000

    #

---
+++ trunk/brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/importbeans/app1/BeanTest2.java
@@ -0,0 +1,8 @@
+package org.brandao.brutos.annotation.helper.importbeans.app1;
+
+import org.brandao.brutos.annotation.Bean;
+
+@Bean
+public class BeanTest2 {
+
+}
---
+++ trunk/brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/importbeans/app1/BeanTest3.java
@@ -0,0 +1,8 @@
+package org.brandao.brutos.annotation.helper.importbeans.app1;
+
+import org.brandao.brutos.annotation.Bean;
+
+@Bean("beanName")
+public class BeanTest3 {
+
+}
