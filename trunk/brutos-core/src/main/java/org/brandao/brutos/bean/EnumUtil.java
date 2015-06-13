a709a101b31940376290fea87b102a77dc1952bb
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2015-06-13 00:06:57 +0000

    #

--- trunk/brutos-core/src/main/java/org/brandao/brutos/bean/EnumUtil.java
+++ trunk/brutos-core/src/main/java/org/brandao/brutos/bean/EnumUtil.java
@@ -20,6 +20,9 @@ package org.brandao.brutos.bean;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.brandao.brutos.BrutosException;
 
 /**
@@ -30,8 +33,21 @@ public class EnumUtil {
 
     private Class enumClass;
 
+    private Map indexMap;
+    
+    private Map nameMap;
+    
     public EnumUtil(Class enumClass){
         this.enumClass = enumClass;
+        this.indexMap = new HashMap();
+        this.nameMap = new HashMap();
+        
+        int maxIndex = this.getEnumConstantLength();
+        for(int i=0;i<maxIndex;i++){
+        	Enum e = (Enum) this.getEnumConstant(new Integer(i));
+        	this.indexMap.put(String.valueOf(e.ordinal()),e);
+        	this.nameMap.put(e.name(),e);
+        }
     }
 
     public Object getEnumConstants(){
@@ -50,10 +66,31 @@ public class EnumUtil {
 
     }
 
-    public Object valueOf( String value ){
+    public Object valueOf(String value){
         return valueOf(this.enumClass,value);
     }
 
+    public Object valueByName(String name){
+        return this.nameMap.get(name);
+    }
+    
+    public Object valueByIndex(String index){
+        return this.indexMap.get(index);
+    }
+    
+    public int getEnumConstantLength(){
+    	return getEnumConstantLength(this.enumClass);
+    }
+
+    public Object getEnumConstant(Integer index){
+    	return getEnumConstant(this.enumClass, index);
+    }
+    
+    public static int getEnumConstantLength(Class enumClazz){
+        Object cons = getEnumConstants(enumClazz);
+        return Array.getLength(cons);
+    }
+    
     public static Object getEnumConstant( Class enumClazz, Integer index ){
         Object cons = getEnumConstants(enumClazz);
         return Array.get(cons, index.intValue());
