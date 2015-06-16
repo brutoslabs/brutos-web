a716ffea3b4bfb4c9ec63a345e974bd24ea5d880
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2015-06-16 18:24:39 +0000

    #

---
+++ brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/temporal/fail/Test4FaiTemporalController.java
@@ -0,0 +1,10 @@
+package org.brandao.brutos.annotation.helper.temporal.fail;
+
+import org.brandao.brutos.annotation.Controller;
+
+@Controller("/controller")
+public class Test4FaiTemporalController {
+
+	private Test2FailBean property;
+	
+}
