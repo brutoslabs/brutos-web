11e0b8ed44270c3afe5f108eaa0bfee5558f8c3a
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2015-06-20 13:43:49 +0000

    #

--- trunk/brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/any/app3/metavaluesdefinition/Test2AnyMetaValuesDefinitionController.java
+++ trunk/brutos-annotation/src/test/java/org/brandao/brutos/annotation/helper/any/app3/metavaluesdefinition/Test2AnyMetaValuesDefinitionController.java
@@ -1,15 +1,12 @@
 package org.brandao.brutos.annotation.helper.any.app3.metavaluesdefinition;
 
-import java.util.List;
 import java.util.Map;
 
 import org.brandao.brutos.annotation.Any;
 import org.brandao.brutos.annotation.Basic;
 import org.brandao.brutos.annotation.Controller;
-import org.brandao.brutos.annotation.ElementCollection;
 import org.brandao.brutos.annotation.KeyCollection;
-import org.brandao.brutos.annotation.MetaValue;
-import org.brandao.brutos.annotation.helper.any.app1.Property;
+import org.brandao.brutos.annotation.helper.any.app3.Property;
 
 @Controller("/controller")
 public class Test2AnyMetaValuesDefinitionController {
