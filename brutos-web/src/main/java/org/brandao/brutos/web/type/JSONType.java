abe12aa97050a8e929283d3aac3df838788a6ad1
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2017-01-26 15:54:05 +0000

    #

--- brutos-web/src/main/java/org/brandao/brutos/web/type/JSONType.java
+++ brutos-web/src/main/java/org/brandao/brutos/web/type/JSONType.java
@@ -4,20 +4,24 @@
 package org.brandao.brutos.web.type;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
 import org.brandao.brutos.BrutosException;
 import org.brandao.brutos.MvcResponse;
 import org.brandao.brutos.type.SerializableType;
-import org.brandao.jbrgates.JSONDecoder;
-import org.brandao.jbrgates.JSONEncoder;
-
+import org.brandao.jbrgates.DefaultJSONContext;
+import org.brandao.jbrgates.JSONContext;
 
 public class JSONType extends SerializableType {
 
+	private static JSONContext jsonContext = new DefaultJSONContext();
+	
     public Object convert(Object value) {
          try{
             if( value instanceof String ){
-                JSONDecoder decoder = new JSONDecoder( (String)value );
-                return decoder.decode( classType );
+                //JSONDecoder decoder = new JSONDecoder( (String)value );
+                //return decoder.decode( classType );
+                return jsonContext.decode( (String)value, classType );
             }
             else
                 return value;
@@ -30,8 +34,12 @@ public class JSONType extends SerializableType {
     public void show(MvcResponse response, Object value) throws IOException {
         response.setType( "application/json" );
         response.setCharacterEncoding( "UTF-8" );
-        JSONEncoder encoder = new JSONEncoder( response.processStream() );
-        encoder.encode( value );
+        //JSONEncoder encoder = new JSONEncoder( response.processStream() );
+        //encoder.encode( value );
+        String str = jsonContext.encode(value);
+        OutputStream out = response.processStream(); 
+        out.write(str.getBytes());
+        out.flush();
     }
 
 }
