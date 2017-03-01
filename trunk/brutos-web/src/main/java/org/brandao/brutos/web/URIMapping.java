21b4948e84669143d32d8d4f35274460a4071a16
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2017-03-01 14:19:38 +0000

    #

--- trunk/brutos-web/src/main/java/org/brandao/brutos/web/URIMapping.java
+++ trunk/brutos-web/src/main/java/org/brandao/brutos/web/URIMapping.java
@@ -1,5 +1,3 @@
-
-
 package org.brandao.brutos.web;
 
 import java.net.MalformedURLException;
@@ -7,47 +5,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+
 import org.brandao.brutos.mapping.StringUtil;
 
 public class URIMapping {
 
     private String originalURI;
     
-    private String uriPattern;
+    private Pattern uriPattern;
 
+    private Pattern uriPrePattern;
+    
     private List<URIParameter> parameters;
 
     public URIMapping( String uri ) throws MalformedURLException{
         this.parse(uri);
-        this.originalURI = uri;
-        this.uriPattern  = getURIPattern(null);
+        this.originalURI   = uri;
+        this.uriPattern    = 
+    		Pattern.compile(this.getURIPattern(), Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+        this.uriPrePattern =
+			Pattern.compile(this.getURIPrePattern(), Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
     }
 
-    /*
-    private void createMap( String uri ) throws MalformedURLException{
-        try{
-            createMap0(uri);
-        }
-        catch(Exception e){
-            throw new MalformedURLException(e.getMessage() + ": " + uri);
-        }
-    }
-    */
-    
     private void parse(String uri) throws MalformedURLException{
-        //fragmentos da uri
+    	
         List<String> frags  = new ArrayList<String>();
-        // identificados detectados
         List<String> ids    = new ArrayList<String>();
-        // regex detectados
         List<String> regexs = new ArrayList<String>();
-    
-        int startFrag      = 0;
-        char[] chars       = uri.toCharArray();
-        int openKeysCount  = 0;
-        int closeKeysCount = 0;
-        int firstOpenKeys  = -1;
-        int lastCloseKeys  = -1;
+        int startFrag       = 0;
+        char[] chars        = uri.toCharArray();
+        int openKeysCount   = 0;
+        int closeKeysCount  = 0;
+        int firstOpenKeys   = -1;
+        int lastCloseKeys   = -1;
 
         for(int i=0;i<chars.length;i++){
         	
@@ -110,114 +101,93 @@ public class URIMapping {
         		frags.add(uri.substring(startFrag, uri.length()));
         }
         
-        //if( frags.size() % 2 == 1 )
-        //    frags.add(null);
 
         parameters = new ArrayList<URIParameter>();
 
         for( int i=0;i<ids.size();i++ ){
-            parameters.add(
-                    new URIParameter(
-                        i,
-                        (String)ids.get(i),
-                        (String)regexs.get(i),
-                        (String)frags.get(i),
-                        (String)frags.get(i+1) ) );
-        }        
-    }
-    
-    /*
-    private void createMap0( String uri ) throws MalformedURLException{
-        //fragmentos da uri
-        List<String> frags = new ArrayList<String>();
-        // identificados detectados
-        List<String> ids   = new ArrayList<String>();
-        // regex detectados
-        List<String> regex = new ArrayList<String>();
-
-        //inicio de um identificador
-        int index = uri.indexOf("{");
-        //identificador
-        String id;
-
-        //se index for igual a -1 entao nao existe identificador. Sua definicao é null
-        //se index for diferente de null entao existe um identificador. E extraido o
-        //fragmento que inicia em 0 e termina em index
-        if( index == -1 )
-            frags.add( null );
-        else
-            frags.add( uri.substring( 0, index ) );
-
-        //enquanto index for diferente de -1, a procura por identificadores continua
-        while( index != -1 ){
-            //fim do identificador
-            int index2 = uri.indexOf("}", index );
-
-            id = index+1 < index2? uri.substring( index+1, index2 ) : null;
-
-
-            if( id == null )
-                throw new MalformedURLException();
-
-            //adiciona o identificador
-            ids.add(getId(id));
-            regex.add(getRegex(id));
-
-            //procura o proximo identificador para obter o proximo fragmento
-            int nextIndex = uri.indexOf( "{", index2 );
-
-            if( nextIndex == -1 ){
-                nextIndex = uri.length();
+        	
+            StringBuilder regexPrefix = new StringBuilder("^");
+            StringBuilder regexSuffix = new StringBuilder("");
+            
+            for(int k=0;k<=i;k++){
+                regexPrefix.append( frags.get(k) == null? "" : Pattern.quote(frags.get(k)) );
+                
+                if(i>0 && k<i){
+                   regexPrefix.append( regexs.get(k) );
+                }
             }
-
-            //fragmento atual
-            String frag = index2+1 < nextIndex? uri.substring(index2+1, nextIndex) : null;
-
-            //adiciona o fragmento
-            frags.add( frag != null? frag.replaceAll("/+", "/") : null );
-
-            index = uri.indexOf("{", index + 1 );
-        }
-
-        //se a quantidade de identificadores for impar, entao o ultimo identificador
-        // foi encontrado no fim da uri
-        if( frags.size() % 2 == 1 )
-            frags.add(null);
-
-        parameters = new ArrayList<URIParameter>();
-
-        for( int i=0;i<ids.size();i++ ){
+        	
+            for(int k=i;k<ids.size();k++){
+            	
+                if(k>i){
+                    regexSuffix.append( regexs.get(k) );
+                }
+                
+                regexSuffix.append(frags.get(k+1) == null? "" : Pattern.quote(frags.get(k+1)) );
+            }
+            
+            regexSuffix.append("$");
+            
             parameters.add(
                     new URIParameter(
                         i,
-                        (String)ids.get(i),
-                        (String)regex.get(i),
-                        (String)frags.get(i),
-                        (String)frags.get(i+1) ) );
+                        ids.get(i),
+                        Pattern.compile(regexs.get(i), Pattern.DOTALL | Pattern.CASE_INSENSITIVE),
+                        frags.get(i),
+                        frags.get(i+1),
+                        Pattern.compile(regexPrefix.toString(), Pattern.DOTALL | Pattern.CASE_INSENSITIVE),
+                        Pattern.compile(regexSuffix.toString(), Pattern.DOTALL | Pattern.CASE_INSENSITIVE)) );
         }
+        
+    }
+
+    private String getURIPrePattern(){
+        StringBuilder result = new StringBuilder();
+        
+        if(parameters.isEmpty())
+            return result.toString();
+        
+        
+        for( int i=0;i<parameters.size();i++ ){
+        	URIParameter p = (URIParameter)parameters.get(i);
+
+            if(i == 0){
+            	
+                if(p.getStart() != null && p.getStart().length() > 0){
+                    result
+                    	.append( (p.getStart() == null? "" : Pattern.quote(p.getStart())) );
+                }
+                
+                result
+                	.append("(")
+                	.append(".*")
+                	.append(")");
+                
+                if(p.getEnd() != null && p.getEnd().length() > 0){
+                    result
+                    	.append( (p.getEnd() == null? "" : Pattern.quote(p.getEnd())) );
+                }
+            }
+            else{
+            	
+                result
+                	.append("(")
+                	.append(".*")
+                	.append(")");
+                
+                if(p.getEnd() != null && p.getEnd().length() > 0){
+                    result
+                    	.append( (p.getEnd() == null? "" : Pattern.quote(p.getEnd())) );
+                }
+            }
+            
+        }
+        
+        return result.toString();
+        
     }
     
-    private String getId(String value) throws MalformedURLException{
-        int index = value.indexOf(":");
-        String result= index == -1? value : value.substring(0,index);
-        
-        if(StringUtil.isEmpty(result))
-            throw new MalformedURLException("invalid parameter id " + value);
-        else
-            return result;
-    }
-
-    private String getRegex(String value){
-        int index = value.indexOf(":");
-        String regex = 
-            index == -1? null : value.substring(index+1,value.length());
-        
-        return StringUtil.isEmpty(regex)? "\\w{1,}" : regex;
-    }
-
-    */
-
-    private String getURIPattern(Object[] params){
+    private String toURI(Object[] params){
         String value = null;
         
         if(parameters.isEmpty())
@@ -226,19 +196,15 @@ public class URIMapping {
         for(int i=0;i<parameters.size();i++ ){
             URIParameter p = parameters.get(i);
             
-            if(i == 0 && p.getStart() != null)
+            if(i == 0 && p.getStart() != null){
                 value = p.getStart();
+            }
 
-            if(params == null)
-                value += p.getRegex();
-            else
-            if(params.length == 0)
-                value += "(" + p.getId() + ")";
-            else
-                value += String.valueOf(params[p.getIndex()]);
+            value += String.valueOf(params[p.getIndex()]);
             
-            if(p.getEnd() != null)
+            if(p.getEnd() != null){
                 value += p.getEnd();
+            }
             
         }
         
@@ -246,35 +212,83 @@ public class URIMapping {
         
     }
     
+    private String getURIWithVars(){
+        String value = null;
+        
+        if(parameters.isEmpty())
+            return this.originalURI;
+        
+        for(int i=0;i<parameters.size();i++ ){
+            URIParameter p = parameters.get(i);
+            
+            if(i == 0 && p.getStart() != null){
+                value = p.getStart();
+            }
+
+            value += "(" + p.getId() + ")";
+            
+            if(p.getEnd() != null){
+                value += p.getEnd();
+            }
+            
+        }
+        
+        return value;
+        
+    }
     
-    public Map<String,List<String>> getParameters(String uri){
-        int start = 0;
-        int end   = 0;
+    private String getURIPattern(){
+        String value = null;
+        
+        if(parameters.isEmpty())
+            return this.originalURI;
+        
+        for(int i=0;i<parameters.size();i++ ){
+            URIParameter p = parameters.get(i);
+            
+            if(i == 0 && p.getStart() != null){
+            	value = Pattern.quote(p.getStart());
+            }
+                value += p.getRegex();
+            
+            if(p.getEnd() != null){
+                value += Pattern.quote(p.getEnd());
+            }
+            
+        }
+        
+        return value;
+        
+    }
+    
+    public Map<String,List<String>> getParameters( String value ){
         Map<String,List<String>> params = new HashMap<String,List<String>>();
 
         for( int i=0;i<parameters.size();i++ ){
-            URIParameter p = (URIParameter)parameters.get(i);
-            start = p.getStart() == null? 0 : uri.indexOf( p.getStart(), start ) + p.getStart().length();
-            end   = p.getEnd() == null? uri.length() : uri.indexOf( p.getEnd(), start + 1 );
-
+        	URIParameter p = parameters.get(i);
+            String tmp     = value;
+            tmp            = p.getRegexPrefix().matcher(tmp).replaceAll("");
+            tmp            = p.getRegexSuffix().matcher(tmp).replaceAll("");
+            
             List<String> values = params.get(p.getId());
+            
             if(values == null){
-            	values = new ArrayList<String>();
-            	params.put(p.getId(), values);
+                values = new ArrayList<String>();
+                params.put(p.getId(), values);
             }
             
-            values.add(uri.substring(start, end));
+            values.add(tmp);
         }
 
         return params;
     }
-
+    
     public String getURI(Object[] params){
-        return getURIPattern(params);
+        return this.toURI(params);
     }
     
-    public boolean matches( String uri ){
-        return uri.matches(this.uriPattern);
+    public boolean matches(String uri){
+    	return this.uriPrePattern.matcher(uri).matches() && this.uriPattern.matcher(uri).matches();
     }
     
 }
