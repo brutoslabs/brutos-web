ce74170a5676e4aabbd92b6143888dafe052d6a0
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2017-05-27 02:49:48 +0000

    #

---
+++ trunk/brutos-annotation/src/main/java/org/brandao/brutos/annotation/configuration/web/WebControllerAnnotationConfig.java
@@ -0,0 +1,43 @@
+package org.brandao.brutos.annotation.configuration.web;
+
+import org.brandao.brutos.ActionType;
+import org.brandao.brutos.ComponentRegistry;
+import org.brandao.brutos.ControllerBuilder;
+import org.brandao.brutos.DispatcherType;
+import org.brandao.brutos.annotation.Controller;
+import org.brandao.brutos.annotation.Stereotype;
+import org.brandao.brutos.annotation.configuration.ControllerAnnotationConfig;
+import org.brandao.brutos.web.WebActionType;
+
+@Stereotype(
+	target = Controller.class,
+	minorVersion = 1
+)
+public class WebControllerAnnotationConfig 
+	extends ControllerAnnotationConfig{
+
+	protected ControllerBuilder registerController(
+			ComponentRegistry componentRegistry, String id, String view,
+			boolean resolvedView, DispatcherType dispatcherType, String name,
+			Class<?> classType, String actionId, ActionType actionType){
+		
+		WebActionType webActionType = 
+				actionType == null? 
+					null : 
+					WebActionType.valueOf(actionType.id());
+		
+		return componentRegistry.registerController(
+				id, view, resolvedView,
+				dispatcherType, name, classType, actionId, webActionType);		
+	}
+	
+	protected ControllerBuilder addAlias(ControllerBuilder builder, String id) {
+		return builder.addAlias(id);
+	}
+	
+	protected String getControllerName(ComponentRegistry componentRegistry,
+			Class<?> controllerClass) {
+		return "/" + super.getControllerName(componentRegistry, controllerClass);
+	}
+	
+}
