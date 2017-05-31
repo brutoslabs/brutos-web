35930bb20bdf7ffdf43632addafc223e10493a4f
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2017-05-31 12:47:56 +0000

    #

--- trunk/brutos-annotation/src/main/java/org/brandao/brutos/annotation/web/ResponseStatus.java
+++ trunk/brutos-annotation/src/main/java/org/brandao/brutos/annotation/web/ResponseStatus.java
@@ -1,3 +1,20 @@
+/*
+ * Brutos Web MVC http://www.brutosframework.com.br/
+ * Copyright (C) 2009-2017 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.brandao.brutos.annotation.web;
 
 import java.lang.annotation.ElementType;
@@ -5,16 +22,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.brandao.brutos.web.HttpStatus;
+import org.brandao.brutos.web.BrutosWebConstants;
 
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ResponseStatus {
 
-	int	code() default HttpStatus.INTERNAL_SERVER_ERROR;
+	int	code() default BrutosWebConstants.DEFAULT_RESPONSE_STATUS;
 
-	String	reason() default "";
-	
-	int	value() default HttpStatus.INTERNAL_SERVER_ERROR;
+	int	value() default BrutosWebConstants.DEFAULT_RESPONSE_STATUS;
 	
 }
--- trunk/brutos-annotation/src/test/java/org/brandao/brutos/web/bean/helper/JsonBeanEncoderControllerTest.java
+++ trunk/brutos-annotation/src/test/java/org/brandao/brutos/web/bean/helper/JsonBeanEncoderControllerTest.java
@@ -1,10 +1,72 @@
 package org.brandao.brutos.web.bean.helper;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.brandao.brutos.annotation.Controller;
+import org.brandao.brutos.annotation.helper.EnumTest;
 
 @Controller("/controller")
 public class JsonBeanEncoderControllerTest {
 
-	public void myAction(JsonBeanEncoderBean p){
+	public JsonBeanEncoderBean myAction(){
+    	JsonBeanEncoderBean o = new JsonBeanEncoderBean();
+    	
+    	o.setPropertyA(10);
+    	o.setPropertyB("value b");
+    	o.setPropertyC(new Date());
+    	o.setPropertyD(new Date());
+    	o.setPropertyE(EnumTest.VALUE1);
+    	o.setPropertyF(EnumTest.VALUE11);
+    	o.setPropertyG(EnumTest.VALUE12);
+    	
+    	Map<String,Integer> h = new HashMap<String, Integer>();
+    	h.put("a", 1);
+    	h.put("b", 2);
+    	
+    	o.setPropertyH(h);
+    	o.setPropertyI(h);
+    	
+    	Map<JsonBeanEncoderInnerBean,Integer> j =
+    			new HashMap<JsonBeanEncoderInnerBean, Integer>();
+    	
+    	JsonBeanEncoderInnerBean k1j = new JsonBeanEncoderInnerBean();
+    	k1j.setProp("k1j");
+
+    	JsonBeanEncoderInnerBean k2j = new JsonBeanEncoderInnerBean();
+    	k2j.setProp("k2j");
+    	
+    	j.put(k1j, 1);
+    	j.put(k2j, 2);
+    	
+    	o.setPropertyJ(j);
+
+    	Map<String, JsonBeanEncoderInnerBean> k =
+    			new HashMap<String, JsonBeanEncoderInnerBean>();
+    	
+    	JsonBeanEncoderInnerBean k1k = new JsonBeanEncoderInnerBean();
+    	k1k.setProp("k1k");
+
+    	JsonBeanEncoderInnerBean k2k = new JsonBeanEncoderInnerBean();
+    	k2k.setProp("k2k");
+
+    	k.put("k1k", k1k);
+    	k.put("k2k", k2k);
+    	
+    	o.setPropertyK(k);
+    	o.setPropertyL(k);
+    	
+    	List<Integer> m = new ArrayList<Integer>();
+    	m.add(1);
+    	m.add(4);
+    	m.add(6);
+    	o.setPropertyM(m);
+    	o.setPropertyN(m);
+    	o.setPropertyO(m);		
+    	
+    	return o;
 	}
 }
