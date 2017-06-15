2fe1a62f2da0b7abd5ac5659d922773165c6e431
plic_ploc <plic_ploc@ad802ddb-d12e-458b-8faf-6c9ae795701d>
2017-06-15 11:53:59 +0000

    #

--- trunk/brutos-annotation/src/main/java/org/brandao/brutos/annotation/configuration/web/WebActionAnnotationConfig.java
+++ trunk/brutos-annotation/src/main/java/org/brandao/brutos/annotation/configuration/web/WebActionAnnotationConfig.java
@@ -4,28 +4,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.brandao.brutos.ActionBuilder;
-import org.brandao.brutos.BrutosException;
 import org.brandao.brutos.ComponentRegistry;
-import org.brandao.brutos.ControllerBuilder;
+import org.brandao.brutos.DataType;
 import org.brandao.brutos.DispatcherType;
-import org.brandao.brutos.annotation.AcceptRequestType;
 import org.brandao.brutos.annotation.Action;
 import org.brandao.brutos.annotation.Controller;
-import org.brandao.brutos.annotation.ResponseType;
-import org.brandao.brutos.annotation.Result;
-import org.brandao.brutos.annotation.ResultView;
 import org.brandao.brutos.annotation.Stereotype;
-import org.brandao.brutos.annotation.View;
 import org.brandao.brutos.annotation.configuration.ActionAnnotationConfig;
 import org.brandao.brutos.annotation.configuration.ActionEntry;
 import org.brandao.brutos.annotation.configuration.ThrowableEntry;
-import org.brandao.brutos.annotation.web.RequestMethod;
 import org.brandao.brutos.annotation.web.ResponseError;
 import org.brandao.brutos.annotation.web.ResponseErrors;
-import org.brandao.brutos.annotation.web.ResponseStatus;
 import org.brandao.brutos.mapping.MappingException;
 import org.brandao.brutos.mapping.StringUtil;
-import org.brandao.brutos.web.MediaType;
 import org.brandao.brutos.web.RequestMethodType;
 import org.brandao.brutos.web.WebActionBuilder;
 import org.brandao.brutos.web.WebControllerBuilder;
@@ -41,137 +32,72 @@ public class WebActionAnnotationConfig
 	protected Object innerApplyConfiguration(Object source, Object builder,
 			ComponentRegistry componentRegistry) {
 
-		ActionEntry method                     = (ActionEntry) source;
+		//vars
+		ActionEntry actionEntry                = (ActionEntry) source;
+		WebActionConfig actionConfig           = new WebActionConfig(actionEntry);
 		WebControllerBuilder controllerBuilder = (WebControllerBuilder) builder;
-		Action action                          = (Action) method.getAnnotation(Action.class);
-		View viewAnnotation                    = method.getAnnotation(View.class);
-		ResultView resultView                  = method.getAnnotation(ResultView.class);
-		AcceptRequestType acceptRequestType    = method.getAnnotation(AcceptRequestType.class);
-		ResponseType responseType              = method.getAnnotation(ResponseType.class);
+		String actionID                        = actionConfig.getActionId();
+		RequestMethodType requestMethodType    = actionConfig.getRequestMethodType();
+		String result                          = actionConfig.getResultActionName();
+		String view                            = actionConfig.getActionView();
+		boolean resultRendered                 = actionConfig.isResultRenderable();
+		boolean rendered                       = actionConfig.isRenderable();
+		boolean resolved                       = actionConfig.isResolvedView();
+		String executor                        = actionConfig.getActionExecutor();
+		DataType[] requestTypes                = actionConfig.getRequestTypes();
+		DataType[] responseTypes               = actionConfig.getResponseTypes();
+		int responseStatus                     = actionConfig.getResponseStatus();
+		DispatcherType dispatcher              = actionConfig.getDispatcherType();
 
-		RequestMethod requestMethod = 
-				method.isAnnotationPresent(RequestMethod.class)?
-					method.getAnnotation(RequestMethod.class) :
-					method.getControllerClass().getAnnotation(RequestMethod.class);
-					
-		String[] actionIDs = 
-				action == null? null : action.value();
-					
-		String[] requestMethodTypes = 
-				requestMethod == null || requestMethod.value().length == 0? 
-						null : 
-						requestMethod.value();
-
-		if(actionIDs != null){
-			for(int i=0;i<actionIDs.length;i++){
-				actionIDs[i] = StringUtil.adjust(action.value()[i]);
-			}
-		}
-		
-		String actionID = 
-				actionIDs == null?
-					null : 
-					StringUtil.adjust(actionIDs[0]);
-
-		RequestMethodType requestMethodType = 
-				requestMethodTypes == null? 
-					null : 
-					RequestMethodType.valueOf(StringUtil.adjust(requestMethodTypes[0]));
-		
-		Result resultAnnotation = method.getAnnotation(Result.class);
-		String result           = resultAnnotation == null ? null : resultAnnotation.value();
-
-		org.brandao.brutos.DispatcherType dispatcher = 
-				viewAnnotation == null? 
-					null : 
-					org.brandao.brutos.DispatcherType.valueOf(StringUtil.adjust(viewAnnotation.dispatcher()));
-
-		ResponseStatus responseStatus = 
-				method.isAnnotationPresent(ResponseStatus.class)?
-					method.getAnnotation(ResponseStatus.class) :
-					method.getControllerClass().getAnnotation(ResponseStatus.class);
-		
-		boolean resultRendered = resultView == null ? false : resultView.rendered();
-		boolean rendered = viewAnnotation == null ? true : viewAnnotation.rendered();
-		boolean resolved = viewAnnotation == null ? false : viewAnnotation.resolved();
-		resolved = rendered ? resolved : true;
-
-		String executor = method.isAbstractAction() ? null : method.getName();
-		String view = getView(method, viewAnnotation, componentRegistry);
-
+		//validtion
 		if (!StringUtil.isEmpty(view) && StringUtil.isEmpty(executor)
 				&& !rendered){
 			throw new MappingException(
 					"view must be rendered in abstract actions: " + actionID);
 		}
 
-		if (method.getReturnType() == void.class) {
-			if (resultAnnotation != null || resultView != null)
-				throw new MappingException("the action not return any value: "
-						+ method.getName());
-		}
-
+		//registry
 		WebActionBuilder actionBuilder = 
-				(WebActionBuilder) controllerBuilder.addAction(actionID, requestMethodType, 
+				(WebActionBuilder)
+				controllerBuilder.addAction(
+						actionID, requestMethodType, 
 					result, resultRendered, view, dispatcher, resolved, executor);
 
-		if(acceptRequestType != null){
-			String[] values = acceptRequestType.value();
-			for(String v: values){
-				actionBuilder.addRequestType(MediaType.valueOf(v));
+		if(requestTypes != null){
+			for(DataType type: requestTypes){
+				actionBuilder.addRequestType(type);
 			}
 		}
-	
-		if(responseType != null){
-			String[] values = responseType.value();
-			for(String v: values){
-				actionBuilder.addResponseType(MediaType.valueOf(v));
+
+		if(responseTypes != null){
+			for(DataType type: responseTypes){
+				actionBuilder.addResponseType(type);
 			}
 		}
+		
+		actionBuilder.setResponseStatus(responseStatus);
+		
+		String[] actionAlias = actionConfig.getAliasName();
+		RequestMethodType[] requestMethodTypeAlias = actionConfig.getRequestMethodTypeAlias();
+		
+		for(String actionName: actionAlias){
+			
+			if(requestMethodTypeAlias.length == 0){
+				actionBuilder.addAlias(actionName);
+			}
+			else{
+				for(RequestMethodType requestMethod: requestMethodTypeAlias){
+					return actionBuilder.addAlias(StringUtil.adjust(actionName), requestMethod);
 					
-		if(responseStatus != null){
-			actionBuilder.setResponseStatus(responseStatus.value());
-		}
-		
-		if(actionIDs != null){
-			for (int i = 1; i < actionIDs.length; i++) {
-				
-				actionIDs[i] = StringUtil.adjust(actionIDs[i]);
-				
-				if(requestMethodType == null){
-					actionBuilder.addAlias(StringUtil.adjust(actionIDs[i]));
-				}
-				else{
-					for(int k = 1; k < requestMethodTypes.length; k++){
-						
-						if (StringUtil.isEmpty(actionIDs[i])){
-							throw new BrutosException("invalid action id: "
-									+ method.getControllerClass().getName() + "."
-									+ method.getName());
-						}
-
-						RequestMethodType rmt = 
-								RequestMethodType.valueOf(
-										StringUtil.adjust(requestMethodTypes[k]));
-						
-						if (rmt == null){
-							throw new BrutosException("invalid request method type: "
-									+ method.getControllerClass().getName() + "."
-									+ method.getName());
-						}
-						
-						return actionBuilder.addAlias(StringUtil.adjust(actionIDs[i]), rmt);
-						
-					}
 				}
 			}
 		}
 		
-		throwsSafe(actionBuilder, method, componentRegistry);
+		throwsSafe(actionBuilder, actionEntry, componentRegistry);
 
-		addParameters(actionBuilder, method, componentRegistry);
+		addParameters(actionBuilder, actionEntry, componentRegistry);
 
-		addResultAction(actionBuilder, method.getResultAction(), componentRegistry);
+		addResultAction(actionBuilder, actionEntry.getResultAction(), componentRegistry);
 		
 		return actionBuilder;
 	}
