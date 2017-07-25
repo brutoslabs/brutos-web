3fda3d01b6b0cff70dcecdf546f8660ca8e2a5bd
Afonso Brandao <6617981+brandaof@users.noreply.github.com>
2017-07-25 12:01:05 +0000

    #

--- brutos-core/src/main/java/org/brandao/brutos/DispatcherType.java
+++ brutos-core/src/main/java/org/brandao/brutos/DispatcherType.java
@@ -26,17 +26,18 @@ import java.util.Map;
  */
 public class DispatcherType {
 
-	public static final DispatcherType INCLUDE = new DispatcherType("include");
+	public static final DispatcherType INCLUDE  = new DispatcherType("include");
 
-	public static final DispatcherType FORWARD = new DispatcherType("forward");
+	public static final DispatcherType FORWARD  = new DispatcherType("forward");
 
 	public static final DispatcherType REDIRECT = new DispatcherType("redirect");
 
-	private static final Map<String, DispatcherType> defaultDispatcher = new HashMap<String, DispatcherType>();
+	private static final Map<String, DispatcherType> defaultDispatcher = 
+			new HashMap<String, DispatcherType>();
 
 	static {
-		defaultDispatcher.put(INCLUDE.toString(), INCLUDE);
-		defaultDispatcher.put(FORWARD.toString(), FORWARD);
+		defaultDispatcher.put(INCLUDE.toString(),  INCLUDE);
+		defaultDispatcher.put(FORWARD.toString(),  FORWARD);
 		defaultDispatcher.put(REDIRECT.toString(), REDIRECT);
 	}
 
@@ -53,9 +54,11 @@ public class DispatcherType {
 	public static DispatcherType valueOf(String value) {
 		if (value == null)
 			return null;
-		else if (defaultDispatcher.containsKey(value))
-			return (DispatcherType) defaultDispatcher.get(value);
+		else
+		if (defaultDispatcher.containsKey(value))
+			return defaultDispatcher.get(value);
 		else
 			return new DispatcherType(value);
 	}
+	
 }
