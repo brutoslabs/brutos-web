6564706dae8c5047ee7257b9d4d19e27d13a41f4
afonso <afonso.rbn@gmail.com>
2018-03-04 12:22:11 -0300

    #

--- brutos-web/src/main/java/org/brandao/brutos/web/WebXMLBrutosConstants.java
+++ brutos-web/src/main/java/org/brandao/brutos/web/WebXMLBrutosConstants.java
@@ -2,7 +2,7 @@ package org.brandao.brutos.web;
 
 public interface WebXMLBrutosConstants {
 
-	public static final String XML_BRUTOS_SCHEMA 		= "brutos_web_1_1.xsd";
+	public static final String XML_BRUTOS_SCHEMA 		= "brutos-web-1.1.xsd";
 
 	public static final String XML_BRUTOS_WEB_NAMESPACE	= "http://www.brutosframework.com.br/schema/web";
 	
