ae78071101c42ac07b2e68f82e46012b0676bba0
afonso <afonso.rbn@gmail.com>
2018-03-26 08:56:02 -0300

    #

--- brutos-core/src/main/java/org/brandao/brutos/mapping/ParameterNameResolver.java
+++ brutos-core/src/main/java/org/brandao/brutos/mapping/ParameterNameResolver.java
@@ -1,26 +1,45 @@
+/*
+ * Brutos Web MVC http://www.brutosframework.com.br/
+ * Copyright (C) 2009-2017 Afonso Brandao. (afonso.rbn@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.brandao.brutos.mapping;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.List;
 
 import org.brandao.brutos.ClassUtil;
 
+/**
+ * 
+ * @author Brandao
+ *
+ */
 public class ParameterNameResolver {
 
 	private static Method getParametersNameMethodByMethod;
 
 	private static Method getParametersNameMethodByCosntructor;
 	
-	private static Method getListItem;
-
 	private static Method getName;
 	
 	static{
 		loadParametersNameMethodByMethod();
 		loadParametersNameMethodByconstructor();
-		loadGetItemListMethod();
 		loadGetNameMethod();
 	}
 	
@@ -48,15 +67,6 @@ public class ParameterNameResolver {
 		}
 	}
 	
-	private static void loadGetItemListMethod(){
-		try{
-			getListItem = List.class.getMethod("get", int.class);
-		}
-		catch(Throwable e){
-			throw new ExceptionInInitializerError(e);
-		}
-	}
-
 	private static void loadGetNameMethod(){
 		try{
 			Class<?> clazz = ClassUtil.get("java.lang.reflect.Parameter");
@@ -123,7 +133,7 @@ public class ParameterNameResolver {
 	
 	private static Object getItem(Object list, int index) 
 			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
-		return getListItem.invoke(list, index);
+		return Array.get(list, index);
 	}
 
 	private static Object getParameterName(Object parameter) 
