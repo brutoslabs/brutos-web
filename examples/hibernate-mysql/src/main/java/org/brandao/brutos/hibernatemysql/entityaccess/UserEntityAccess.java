ca202a3088bfb6fda01361afe0486bbda2f9be5a
afonso <afonso.rbn@gmail.com>
2018-04-01 20:59:20 -0300

    #

---
+++ examples/hibernate-mysql/src/main/java/org/brandao/brutos/hibernatemysql/entityaccess/UserEntityAccess.java
@@ -0,0 +1,8 @@
+package org.brandao.brutos.hibernatemysql.entityaccess;
+
+import org.brandao.brutos.hibernatemysql.entity.User;
+
+public interface UserEntityAccess 
+	extends EntityAccess<User>{
+	
+}
