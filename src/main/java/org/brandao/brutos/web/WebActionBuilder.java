54ee0e27cafdbaf793a907d42b22094aef7e585c
Brandao <Brandao@git.uoutec.com.br>
2024-05-15 10:38:51 -0300

    NPE ao instanciar uma especialização de WebActionBuilder

--- src/main/java/org/brandao/brutos/web/WebActionBuilder.java
+++ src/main/java/org/brandao/brutos/web/WebActionBuilder.java
@@ -45,9 +45,9 @@ public class WebActionBuilder extends ActionBuilder{
     
 	private ConfigurableWebApplicationContext webApplicationContext;
 	
-    public WebActionBuilder(ActionBuilder builder, ConfigurableApplicationContext applicationContext){
+    public WebActionBuilder(ActionBuilder builder){
         super(builder);
-        this.webApplicationContext = (ConfigurableWebApplicationContext) applicationContext;
+        this.webApplicationContext = (ConfigurableWebApplicationContext)applicationContext;
     }
     
     public WebActionBuilder(
