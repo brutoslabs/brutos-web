ca0a3c831f7d1c179a970be94178a7fb11161d7f
Brandao <afonso.rbn@gmail.com>
2025-10-28 03:26:32 -0300

    request body cache

--- src/main/java/org/brandao/brutos/web/RequestBody.java
+++ src/main/java/org/brandao/brutos/web/RequestBody.java
@@ -1,12 +1,39 @@
 package org.brandao.brutos.web;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class RequestBody {
 
 	private StreamCache streamCache;
 
-	public RequestBody(StreamCache streamCache) {
+	private String charset;
+	
+	public RequestBody(StreamCache streamCache, String charset) {
 		this.streamCache = streamCache;
 	}
 	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(streamCache.getInputStream(),charset))){
+			char[] b = new char[2048];
+			int l;
+			while((l = br.read(b, 0, b.length)) != -1) {
+				sb.append(b, 0, l);
+			}
+		}
+		catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+		
+		return sb.toString();
+	}
+	
+	public InputStream getInputStream() throws IOException {
+		return streamCache.getInputStream();
+	}
 	
 }
